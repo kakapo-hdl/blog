a3c0c71ffed650918b09c218e302fd1e1874a587
kakapo <64876593+kakapo-hdl@users.noreply.github.com>
2021-11-03 13:43:51 +0800

    ck

--- blog-font-end/src/types/@ckeditor/ckeditor5-react/index.d.ts
+++ blog-font-end/src/types/@ckeditor/ckeditor5-react/index.d.ts
@@ -1,4 +1,7 @@
 declare module '@ckeditor/ckeditor5-react';
 declare module '@ckeditor/ckeditor5-build-classic';
-declare module '@ckeditor/ckeditor5-build-classic/build/translations/zh-cn';
 declare module '@ckeditor/ckeditor5-build-decoupled-document';
+declare module '@ckeditor/ckeditor5-export-word/src/exportword'
+declare module '@ckeditor/ckeditor5-build-classic/build/translations/zh-cn';
+declare module '@ckeditor/ckeditor5-image/src/image';
+
